728edac8ce752deae1a35b4be83437818f3f1c09
Victor <rusuvictor30@gmail.com>
2020-10-18 18:58:17 -0400

    Final commit

--- back-end/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, 'https://www.google.com/search?q=sport+news');
-curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($curl);
-curl_close($curl);
-?>
