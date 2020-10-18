d760febbc4b19df51505a87b4dc2e7f348266aba
Victor <rusuvictor30@gmail.com>
2020-10-18 14:36:26 -0400

    Days added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,7 +6,7 @@ import marker from './assets/marker.png';
 function App() {
   return (
     <div className="App">
-      <svg id="mainwidget" width="950" x="0" y="0" height="200" xmlnsXlink="http://www.w3.org/2000/svg"
+      <svg id="mainwidget" width="794" x="0" y="0" height="200" xmlnsXlink="http://www.w3.org/2000/svg"
      xlinkHref="http://www.w3.org/1999/xlink">
         <g>
           <g className="currentWeather">
@@ -14,13 +14,13 @@ function App() {
             <rect x="0" y="0" width="158" height="30" className="currentDate"></rect>
             <text x="15" y="21" className="currentDateText">SUNDAY @ 9:37 AM</text>
             <rect x="0" y="30" width="158" height="35" className="currentTemp"></rect>
-            <text className="currentTempText" x="60" y="55">50&#8457;</text>
+            <text className="currentTempText" x="60" y="55">65&#8457;</text>
             <rect x="0" y="65" width="158" height="100" className="currentWind"></rect>
             <image xlinkHref={windImg} x="30" y="72" width="102" height="100"></image>
             <text className="currentWindText" x="66" y="125">14</text>
             <image transform="rotate(210.39999999999998, 135.03127511445913, 178.4379192527066)" width="20" height="20" x="185" y="215" xlinkHref={marker}></image>
             <rect x="0" y="165" width="158" height="35" className="currentWater"></rect>
-            <text className="currentWaterText" x="40" y="189">WATER: 30℉</text>
+            <text className="currentWaterText" x="40" y="189">WATER: 40℉</text>
           </g>
           <rect x="158" y="0" width="1" height="200" fill="white"></rect>
           <g>
@@ -29,26 +29,28 @@ function App() {
           </g>
           <g>
             <rect x="159" y="30" width="158" height="170" className="dayBlock"></rect>
+            <rect x="159" y="30" width="158" height="35" className="day"></rect>
+            <text x="215" y="52" className="currentDateText">Monday</text>
+            
           </g>
           <g>
             <rect x="317" y="30" width="1" height="170" fill="white"></rect>
             <rect x="318" y="30" width="158" height="170" className="dayBlock"></rect>
+            <rect x="318" y="30" width="158" height="35" className="day"></rect>
+            <text x="370" y="52" className="currentDateText">Tuesday</text>
           </g>
           <g>
             <rect x="476" y="30" width="1" height="170" fill="white"></rect>
             <rect x="477" y="30" width="158" height="170" className="dayBlock"></rect>
+            <rect x="477" y="30" width="158" height="35" className="day"></rect>
+            <text x="520" y="52" className="currentDateText">Wednesday</text>
           </g>
           <g>
             <rect x="635" y="30" width="1" height="170" fill="white"></rect>
             <rect x="636" y="30" width="158" height="170" className="dayBlock"></rect>
+            <rect x="636" y="30" width="158" height="35" className="day"></rect>
+            <text x="690" y="52" className="currentDateText">Thursday</text>
           </g>
-          <g>
-            <rect x="794" y="30" width="1" height="170" fill="white"></rect>
-            <rect x="795" y="30" width="158" height="170" className="dayBlock"></rect>
-          </g>
-
-
-
 
           <linearGradient x1="0%" y1="0%" x2="100%" y2="0%" id="wsGradient">
             <stop offset="0%" className="cl2"></stop>
@@ -63,7 +65,7 @@ function App() {
             <stop offset="50.229508196721312%" className="cl2"></stop>
           </linearGradient>
           <symbol id="heatLine">
-            <svg viewBox="-16.8 0 100 50" preserveAspectRatio="none" >
+            <svg viewBox="-20 0 100 50" preserveAspectRatio="none" >
               <g>
                 <path d="M0,30 v20 h100 v-20 Q70,10 50,30 T0,30" 
                   className="heatLine" />
